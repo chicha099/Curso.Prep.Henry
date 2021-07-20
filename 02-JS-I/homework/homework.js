888a15a88235248b04a20983a1e9ba37f1910adb
Florengo <florsg44713@gmail.com>
2021-07-19 22:25:45 -0300

    prueba

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -15,7 +15,7 @@ const nuevaResta = 10 - 5 === 5;
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
-// Resuelve el siguiente problema matemático:
+// Resuelve el siguiente problema matemático: ||sisi
 const nuevoModulo = 21 % 5 === 1;
 
 
